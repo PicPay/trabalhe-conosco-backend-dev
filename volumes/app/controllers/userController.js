8b9344e848cdfd722bf8b409a4a83d44bcf0f723
mateusvtt@gmail.com <mateusvtt@gmail.com>
2017-08-16 18:42:44 -0300

    Iniciando criacao de atributo de tags

--- volumes/app/controllers/userController.js
+++ volumes/app/controllers/userController.js
@@ -10,13 +10,26 @@ router.post('/', function (req, res) {
   User.create({
     id:   req.body.id,
     name : req.body.name,
-    name : req.body.username,
+    username : req.body.username,
   },
   function (err, user) {
     if (err) return res.status(500).send("There was a problem adding the information to the database.");
     res.status(200).send(user);
   });
 });
+
+router.get('/', function (req, res) {
+  User.find({}, function (err, users) {
+    if (err) return res.status(500).send("There was a problem finding the users.");
+    res.send(users);
+  });
+});
+
+// router.get('/update', function (req, res) {
+//   User.updateAll(function(callback){
+//     console.log("atualizados");
+//   });
+// });
 
 router.get('/:id', function (req, res) {
   User.findById(req.params.id, function (err, user) {
@@ -37,5 +50,15 @@ router.put('/:id', function (req, res) {
   User.findByIdAndUpdate(req.params.id, req.body, {new:true}, function (err, user) {
     if (err) return res.status(500).send("There was a problem updating the user.");
     res.status(200).send(user);
+  });
+});
+
+
+router.get('/teste/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    user.createTagsField(req.params.id, function (err){
+      if (err) return res.status(500).send("There was a problem deleting the user.");
+      res.status(200).send("User "+ user.name +" was deleted.");
+    });
   });
 });
--- volumes/app/models/user.js
+++ volumes/app/models/user.js
@@ -1,9 +1,71 @@
 var mongoose = require('mongoose');
+var async = require('async');
 var userSchema = new mongoose.Schema({
-  id: {type: String, unique: true},
+  id: { type: String, unique: true, index:true },
   name: String,
   username: String,
+  tags: [String],
 });
+
+userSchema.statics.updateAll  = function (callback){
+  this.update({}, {name:'topzeira'},{multi: true},
+      function(err, num) {
+          console.log("updated "+num);
+          return callback();
+      }
+  );
+};
+
+userSchema.methods.createTagsField = function (callback){ //cria o campo tags para otimizar a busca
+  console.log("entrei aqui",this);
+  var name = this.name;
+  var username = this.username;
+  async.parallel([ //padronizando as strings de username e user em paralelo
+    function(cb) {
+      standardizeString(name,function(err,stdName){ //padroniza name
+        if (err) return cb(err);
+        name=stdName;
+        return cb();
+      });
+    },
+    function(cb) {
+      standardizeString(username,function(err,stdUsername){ //padroniza username
+        if (err) return cb(err);
+        username=stdUsername;
+        return cb();
+      });
+    }
+  ], function(err) { //terminou de padronizar as duas, insere o field tags no elemento
+    if (err) return next(err);
+    removeDuplicatesFromArrays(name,username,function (err,tags) { //remove elementos repetidos
+      console.log(tags);
+      var query = { _id: this._id };
+      var update = { $set: { name: 'howMany' }};
+      this.constructor.update(query, update, callback);
+    });
+  });
+};
+
+function removeDuplicatesFromArrays(array1,array2,callback){
+  var a = new Set(array1);
+  var b = new Set(array2);
+  var onlyArray1Has = (new Set([...a].filter(x => !b.has(x)))); //a\b
+  var onlyArray2Has = (new Set([...b].filter(x => !a.has(x)))); //b\a
+  var bothArraysHas = (new Set([...a].filter(x => b.has(x))));  //a∩b
+  var arrStd = new Set([...onlyArray1Has, ...onlyArray2Has, ...bothArraysHas])
+  return callback(null,arrStd);
+};
+
+function standardizeString (str,callback){
+  str = str.toLowerCase(); //coloca em minusculo
+  str = str.split(/[ .]+/); //remove pontos e espacos
+  var temp = [];
+  for(let i of str)
+      i && temp.push(i); // copia os valores nao vazios para um array temporario
+  str = temp;
+  delete temp; // discard the variable
+  return callback(null,str);
+};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
