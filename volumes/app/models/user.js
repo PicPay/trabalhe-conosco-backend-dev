63ab7c435f3e2bdb82a1392854fd151b7783287c
Mateus Tanure <mateusvtt@gmail.com>
2017-08-17 00:23:17 -0300

    Modularizando funções de user

--- volumes/app/models/user.js
+++ volumes/app/models/user.js
@@ -1,17 +1,18 @@
 var mongoose = require('mongoose');
-var async = require('async');
 var userSchema = new mongoose.Schema({
   id: { type: String, unique: true, index:true },
   name: String,
   username: String,
   tags: { type: [String], index:true },
+  lista1: { type: Number, default: 0 },
+  lista2: { type: Number, default: 0 },
 });
 
 userSchema.statics.updateAll  = function (callback){
-  console.log("aqui");
+  var functions = require('../userFunctions');
   this.find({}).stream()
      .on('data', function(user){
-        createTagsField(user.name,user.username,function(err,tags){
+        userFunctions.createTagsField(user.name,user.username,function(err,tags){
           user.set('tags', tags);
           user.save(function (error) {
             if (error) throw error;
@@ -26,52 +27,6 @@ userSchema.statics.updateAll  = function (callback){
       console.log("TERMINEI DE ATUALIZAR");
       return callback();
   });
-};
-
-function createTagsField(name,username,callback){ //cria o campo tags para otimizar a busca
-  async.parallel([ //padronizando as strings de username e user em paralelo
-    function(cb) {
-      standardizeString(name,function(err,stdName){ //padroniza name
-        if (err) return cb(err);
-        name=stdName;
-        return cb();
-      });
-    },
-    function(cb) {
-      standardizeString(username,function(err,stdUsername){ //padroniza username
-        if (err) return cb(err);
-        username=stdUsername;
-        return cb();
-      });
-    }
-  ], function(err) { //terminou de padronizar as duas, insere o field tags no elemento
-    if (err) return next(err);
-    removeDuplicatesFromArrays(name,username,function (err,tags) { //remove elementos repetidos
-      return callback(null,tags);
-    });
-  });
-};
-
-function removeDuplicatesFromArrays(array1,array2,callback){
-  var a = new Set(array1);
-  var b = new Set(array2);
-  var onlyArray1Has = (new Set([...a].filter(x => !b.has(x)))); //a\b
-  var onlyArray2Has = (new Set([...b].filter(x => !a.has(x)))); //b\a
-  var bothArraysHas = (new Set([...a].filter(x => b.has(x))));  //a∩b
-  var arrStd = new Set([...onlyArray1Has, ...onlyArray2Has, ...bothArraysHas])
-  arrStd = [...arrStd]; //convertendo set em array
-  return callback(null,arrStd);
-};
-
-function standardizeString (str,callback){
-  str = str.toLowerCase(); //coloca em minusculo
-  str = str.split(/[ .]+/); //remove pontos e espacos
-  var temp = [];
-  for(let i of str)
-      i && temp.push(i); // copia os valores nao vazios para um array temporario
-  str = temp;
-  delete temp; // discard the variable
-  return callback(null,str);
 };
 
 var User = mongoose.model('User', userSchema);
