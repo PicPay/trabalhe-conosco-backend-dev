c1b060a313554abc60e2726e8bd2c5968fc3b0f8
Fabiano Amaral <fabianin.amaral@gmail.com>
2017-08-26 20:43:29 -0300

    mudança na estratégia de indexação

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
 \c docker;
-CREATE EXTENSION pg_trgm;
+-- CREATE EXTENSION pg_trgm;
 CREATE SCHEMA IF NOT EXISTS picpay;
 CREATE TABLE picpay.registers(  
    id UUID PRIMARY KEY,
@@ -16,16 +16,9 @@ CREATE TABLE picpay.rank2(
 COPY picpay.registers FROM '/docker-entrypoint-initdb.d/users.csv' CSV;
 COPY picpay.rank1 FROM '/docker-entrypoint-initdb.d/lista1.csv' CSV;
 COPY picpay.rank2 FROM '/docker-entrypoint-initdb.d/lista2.csv' CSV;
--- CREATE INDEX name_index_text
---     ON picpay.registers USING btree
---     (name text_pattern_ops ASC NULLS LAST)
---     TABLESPACE pg_default;
+-- TABLESPACE pg_default;
 
-CREATE INDEX tbl_col_gin_trgm_idx ON picpay.registers USING gin (name gin_trgm_ops);
-CREATE INDEX tbl_col_gin_trgm_idz ON picpay.registers USING gin (username gin_trgm_ops);
-
-
--- CREATE INDEX username_index_text
---     ON picpay.registers USING btree
---     (username text_pattern_ops ASC NULLS LAST)
---     TABLESPACE pg_default;
+-- CREATE INDEX tbl_col_gin_trgm_idx ON picpay.registers USING gin (lower(name gin_trgm_ops));
+-- CREATE INDEX tbl_col_gin_trgm_idz ON picpay.registers USING gin (username gin_trgm_ops);
+CREATE INDEX ix_users_name_lower ON picpay.registers (lower(name) varchar_pattern_ops);
+CREATE INDEX ix_users_username_lower ON picpay.registers (username varchar_pattern_ops);
