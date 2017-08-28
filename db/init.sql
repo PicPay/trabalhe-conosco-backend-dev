9d6c1076abeb5619abf63da9ba090b8a72fdcf76
Fabiano Amaral <fabianin.amaral@gmail.com>
2017-08-28 13:10:23 -0300

    modificado indexacao

--- db/init.sql
+++ db/init.sql
@@ -18,7 +18,7 @@ COPY picpay.rank1 FROM '/docker-entrypoint-initdb.d/lista1.csv' CSV;
 COPY picpay.rank2 FROM '/docker-entrypoint-initdb.d/lista2.csv' CSV;
 -- TABLESPACE pg_default;
 
-CREATE INDEX tbl_col_gin_trgm_idx ON picpay.registers USING gin (name gin_trgm_ops);
+CREATE INDEX tbl_col_gin_trgm_idx ON picpay.registers USING gin (lower(name) gin_trgm_ops);
 CREATE INDEX tbl_col_gin_trgm_idz ON picpay.registers USING gin (username gin_trgm_ops);
 CREATE INDEX ix_users_name_lower ON picpay.registers (lower(name) varchar_pattern_ops);
 CREATE INDEX ix_users_username_lower ON picpay.registers (username varchar_pattern_ops);
