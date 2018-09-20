4724635fc9b854aff1a94f19543a3b6f9cc2ef2f
Brunoedusb <brunoed@ucl.br>
2018-09-20 12:58:07 -0300

    split users

--- api/database/seeds/UsersTableSeeder.php
+++ api/database/seeds/UsersTableSeeder.php
@@ -104,4 +104,32 @@ class UsersTableSeeder extends Seeder
         fclose ($handle);
     }
 
+    function fsplit($file,$buffer=1024){
+        //get file size
+        $file_size = filesize($file);
+        //no of parts to split
+        $parts = $file_size / $buffer;
+    
+        //store all the file names
+        $file_parts = array();
+    
+        for($i=0;$i<$parts;$i++){
+            //open file to read buffer sized amount from file
+            $file_part = fread($file_handle = fopen($file,'r'), $buffer);
+            //path to write the final files the filename of the part
+            $file_part_path = "users/".$i.".csv";
+            //open the new file [create it] to write
+            $file_new = fopen($file_part_path,'w+');
+            //write the part of file
+            fwrite($file_new, $file_part);
+            //add the name of the file to part list [optional]
+            array_push($file_parts, $file_part_path);
+            //close the part file handle
+            fclose($file_new);
+        }    
+        //close the main file handle
+        fclose($file_handle);
+        return $file_parts;
+    }
+
 }
