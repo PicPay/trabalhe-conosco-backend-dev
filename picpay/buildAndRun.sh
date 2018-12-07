9853532f4f9d2e009ceb39baaf9cc81b08644a5d
Breno Pessoa <brenopessoamelo@gmail.com>
2018-12-07 16:17:13 -0300

    aprimorando buildAndRun
    Signed-off-by: Breno Pessoa <brenopessoamelo@gmail.com>

--- picpay/buildAndRun.sh
+++ picpay/buildAndRun.sh
@@ -1,3 +1,6 @@
+wget -c https://s3.amazonaws.com/careers-picpay/users.csv.gz
+gzip -d users.csv.gz
+mv users.csv  src/main/resources/
 mvn clean package -DskipTests
 docker-compose build
 docker-compose up
