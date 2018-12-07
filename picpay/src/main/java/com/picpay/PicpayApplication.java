18369a7b68f1411542670b3cdb313fb7a3792cce
Breno Pessoa <brenopessoamelo@gmail.com>
2018-12-07 10:15:35 -0300

    ArquivoUtils e configuracoes inicias.

--- picpay/src/main/java/com/picpay/PicpayApplication.java
+++ picpay/src/main/java/com/picpay/PicpayApplication.java
@@ -2,8 +2,16 @@ package com.picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableAsync
+@EnableSpringDataWebSupport
+@EnableMongoRepositories(basePackages = "com.br.picpay.repository")
+@EnableWebSecurity
 public class PicpayApplication {
 
 	public static void main(String[] args) {
