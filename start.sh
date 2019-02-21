bbd6f4332d96ca227643c8f0a41bd573a32fbe18
vtferrari <vtferrari@hotmail.com>
2019-02-21 13:50:44 -0300

    Adicionado configuraçõs de conexão com elasticSearch

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker-compose -f ./infra/docker-compose.yml down
+docker-compose -f ./infra/docker-compose.yml up -d --build elasticsearch
+echo "Wait for ElasticSearch"
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9200)" != "200" ]]; do sleep 1; echo "#"; done; echo "done"'
+
+./mvnw clean install
+
+
+docker-compose -f ./infra/docker-compose.yml up -d --build
