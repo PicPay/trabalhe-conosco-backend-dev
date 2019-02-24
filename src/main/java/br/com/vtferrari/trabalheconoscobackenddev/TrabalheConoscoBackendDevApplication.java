54cae1e4d574ab7406c0807dce1aa62219f8da63
vtferrari <vtferrari@hotmail.com>
2019-02-24 16:40:13 -0300

    Removido pasta front

--- src/main/java/br/com/vtferrari/trabalheconoscobackenddev/TrabalheConoscoBackendDevApplication.java
+++ src/main/java/br/com/vtferrari/trabalheconoscobackenddev/TrabalheConoscoBackendDevApplication.java
@@ -8,7 +8,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 @SpringBootApplication
 public class TrabalheConoscoBackendDevApplication {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(TrabalheConoscoBackendDevApplication.class, args);
     }
 
