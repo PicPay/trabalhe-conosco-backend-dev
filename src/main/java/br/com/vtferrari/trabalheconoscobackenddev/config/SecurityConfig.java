56bba5b4f6fe92e770cd8e14d941dc55ac77dd4b
vtferrari <vtferrari@hotmail.com>
2019-02-23 21:58:52 -0300

    Adicionado nivel basico de Autenticação

---
+++ src/main/java/br/com/vtferrari/trabalheconoscobackenddev/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.vtferrari.trabalheconoscobackenddev.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
